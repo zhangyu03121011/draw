262a82a1be18fa1841f03cc72a080d09e835e88d
bb <753450350@qq.com>
2017-08-23 11:59:41 +0800

    update

---
+++ JSON/myJson.js
@@ -0,0 +1,327 @@
+/**
+ * Created by Lbkj on 2017/08/17.
+ */
+
+var ss = {
+    "code": null,
+    "msg": null,
+    "status": true,
+    "data": {
+        "activityId": "00debd2483aa4f0eb10fcaa54df041be",
+        "activityName": "暑期狂欢",
+        "awardsList": [
+            "iphone 6S",
+            "谢谢参与",
+            "itouch 3",
+            "谢谢参与",
+            "shffel 3",
+            "谢谢参与"
+        ]
+    }
+};
+
+
+var sss = {
+    "code": null,
+    "msg": null,
+    "status": true,
+    "data": {
+        "id": null,
+        "activityId": "00debd2483aa4f0eb10fcaa54df041be",
+        "memberId": null,
+        "awardId": null,
+        "status": null,
+        "delFlag": null,
+        "createDate": null,
+        "updateDate": null,
+        "awardIndex": "2",
+        "awardName": "谢谢参与",
+        "prize": false
+    }
+};
+
+var s = {
+    "code": null,
+    "msg": "用户已抽奖次数超过活动限制次数【10】",
+    "status": false,
+    "data": null
+};
+
+var oo = {
+    //中奖榜单
+    "code": null,
+    "msg": null,
+    "status": true,
+    "data": [
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502958339000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": "http://wx.qlogo.cn/mmopen/TTQibyKjricky34IplhW5C20ibjFsDzxcxzq2SsM9wiaRIW5xR7S60wDAOuI0n2zkicBQUdjQEq4TnAMrseib5lgA0RxVTnVTVXXKw/0",
+            "activityName": null,
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502959624000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": "http://wx.qlogo.cn/mmopen/TTQibyKjricky34IplhW5C20ibjFsDzxcxzq2SsM9wiaRIW5xR7S60wDAOuI0n2zkicBQUdjQEq4TnAMrseib5lgA0RxVTnVTVXXKw/0",
+            "activityName": null,
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502958415000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": "http://wx.qlogo.cn/mmopen/TTQibyKjricky34IplhW5C20ibjFsDzxcxzq2SsM9wiaRIW5xR7S60wDAOuI0n2zkicBQUdjQEq4TnAMrseib5lgA0RxVTnVTVXXKw/0",
+            "activityName": null,
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502959770000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": "http://wx.qlogo.cn/mmopen/TTQibyKjricky34IplhW5C20ibjFsDzxcxzq2SsM9wiaRIW5xR7S60wDAOuI0n2zkicBQUdjQEq4TnAMrseib5lgA0RxVTnVTVXXKw/0",
+            "activityName": null,
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502959039000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": "http://wx.qlogo.cn/mmopen/TTQibyKjricky34IplhW5C20ibjFsDzxcxzq2SsM9wiaRIW5xR7S60wDAOuI0n2zkicBQUdjQEq4TnAMrseib5lgA0RxVTnVTVXXKw/0",
+            "activityName": null,
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502959661000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": "http://wx.qlogo.cn/mmopen/TTQibyKjricky34IplhW5C20ibjFsDzxcxzq2SsM9wiaRIW5xR7S60wDAOuI0n2zkicBQUdjQEq4TnAMrseib5lgA0RxVTnVTVXXKw/0",
+            "activityName": null,
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502958392000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": "http://wx.qlogo.cn/mmopen/TTQibyKjricky34IplhW5C20ibjFsDzxcxzq2SsM9wiaRIW5xR7S60wDAOuI0n2zkicBQUdjQEq4TnAMrseib5lgA0RxVTnVTVXXKw/0",
+            "activityName": null,
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502959641000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "itouch 3",
+            "avatar": "http://wx.qlogo.cn/mmopen/TTQibyKjricky34IplhW5C20ibjFsDzxcxzq2SsM9wiaRIW5xR7S60wDAOuI0n2zkicBQUdjQEq4TnAMrseib5lgA0RxVTnVTVXXKw/0",
+            "activityName": null,
+            "prize": false
+        }
+    ]
+}
+
+var myJpList = {
+    "code": null,
+    "msg": null,
+    "status": true,
+    "data": [
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502958339000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": null,
+            "activityName": "暑期狂欢",
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502959624000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": null,
+            "activityName": "暑期狂欢",
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502958415000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": null,
+            "activityName": "暑期狂欢",
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502959770000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": null,
+            "activityName": "暑期狂欢",
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502959039000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": null,
+            "activityName": "暑期狂欢",
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502959661000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": null,
+            "activityName": "暑期狂欢",
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502958392000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "shffel 3",
+            "avatar": null,
+            "activityName": "暑期狂欢",
+            "prize": false
+        },
+        {
+            "id": null,
+            "activityId": null,
+            "memberId": null,
+            "awardId": null,
+            "status": "1",
+            "delFlag": null,
+            "createDate": 1502959641000,
+            "updateDate": null,
+            "awardIndex": null,
+            "awardName": "itouch 3",
+            "avatar": null,
+            "activityName": "暑期狂欢",
+            "prize": false
+        }
+    ]
+}
+
+var pplk = {
+    //点击抽奖接口返回数据
+    "code": null,
+    "msg": null,
+    "status": true,
+    "data": {
+        "id": null,
+        "activityId": "00debd2483aa4f0eb10fcaa54df041be",
+        "memberId": null,
+        "awardId": null,
+        "status": null,
+        "delFlag": null,
+        "createDate": null,
+        "updateDate": null,
+        "awardIndex": "2",
+        "awardName": "谢谢参与",
+        "avatar": null,
+        "activityName": null,
+        "prize": false
+    }
+}
